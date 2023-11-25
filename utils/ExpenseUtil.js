33ff1ae8c5b56c0f7a167ecc1e02ae1f45a917ed
SwanHtetAungX <contact.swanhtetaung@gmail.com>
2023-11-25 21:24:24 +0800

    Implement edit-expense

--- utils/ExpenseUtil.js
+++ utils/ExpenseUtil.js
@@ -16,6 +16,7 @@ async function readJSON(filename) {
   }
 }
 
+
 // Function to  write an object to a JSON file
 async function writeJSON(object, filename) {
   try {
@@ -36,6 +37,7 @@ async function writeJSON(object, filename) {
     throw err;
   }
 }
+
 
 // Function to add an expense to the JSON file and return the updated expenses
 async function addExpense(req, res) {
@@ -62,7 +64,64 @@ async function addExpense(req, res) {
   }
 }
 
-// Exporting the 'addExpense' function to make it available for external use
+
+
+// function to handle the editing of an expense based on its ID
+async function editExpense(req, res) {
+    try {
+        // Extracting parameters from the request
+        const id = req.params.id;
+        const name = req.body.name;
+        const description = req.body.description;
+        const amount = req.body.amount;
+        const category = req.body.category;
+        const username = req.body.username;
+        const date = req.body.date;
+
+        // Reading all expenses from the JSON file
+        const allExpenses = await readJSON('utils/transactions.json');
+
+        // Variable to track if any modification has occurred
+        var modified = false;
+
+        // Iterating through all expenses to find and update the specified expense by ID
+        for (var i = 0; i < allExpenses.length; i++) {
+            var currentExpense = allExpenses[i];
+            if (currentExpense.id == id){
+                // Modifying the properties of the matching expense
+                allExpenses[i].name = name;
+                allExpenses[i].description = description;
+                allExpenses[i].amount = amount;
+                allExpenses[i].category = category;
+                allExpenses[i].username = username;
+                allExpenses[i].date = date;
+
+                // Setting the 'modified' flag to true
+                modified = true;
+            }   
+        }
+
+        // Checking if any modification has occurred
+        if (modified) {
+            // Writing the updated expenses back to the JSON file
+            await fs.writeFile('utils/transactions.json', JSON.stringify(allExpenses), 'utf8');
+            
+            // Sending a success response with a 201 status code
+            return res.status(201).json({ message: 'Expense modified successfully!' });
+        } else {
+            // Sending an error response if no modification occurred
+            return res.status(500).json({ message: 'Error occurred, unable to modify!' });
+        }
+    } catch (error) {
+        // Handling errors during the expense modification process
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
+// Exporting functions
 module.exports = {
   addExpense,
+  editExpense
+
 };
