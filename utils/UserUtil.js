3e649b617ec4ee09782274a4d4f548ca452be2f5
SwanHtetAungX <contact.swanhtetaung@gmail.com>
2023-11-27 17:01:42 +0800

    Testing Hound CI pull request

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -2,10 +2,10 @@ const { User } = require("../models/User");
 const fs = require("fs").promises;
 async function readJSON(filename) {
   try {
-       const data = await fs.readFile(filename, "utf8");
+    const data = await fs.readFile(filename, "utf8");
     return JSON.parse(data);
   } catch (err) {
-    console.error(err);
+    console.error(err)
     throw err;
   }
 }
