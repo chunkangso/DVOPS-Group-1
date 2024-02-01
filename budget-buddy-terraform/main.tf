67e705910bd012a83504669361946a04204eb21b
SwanHtetAungX <contact.swanhtetaung@gmail.com>
2024-02-01 19:38:27 +0800

    Completed Iac Configuration using Terraform

--- budget-buddy-terraform/main.tf
+++ budget-buddy-terraform/main.tf
@@ -8,22 +8,22 @@ source = "hashicorp/azurerm"
 provider "azurerm" {
 features {}
 }
-resource "azurerm_resource_group" "dvopsResourceGroup" {
-name = "dvopsResourceGroup"
+resource "azurerm_resource_group" "dvopsResourceGroupNew" {
+name = "dvopsResourceGroupNew"
 location = "East US"
 }
 resource "azurerm_kubernetes_cluster" "dvopsAKSCluster" {
 name = "dvopsAKSCluster"
-location = azurerm_resource_group.dvopsResourceGroup.location
-resource_group_name = azurerm_resource_group.dvopsResourceGroup.name
-dns_prefix = "rms-aks"
+location = azurerm_resource_group.dvopsResourceGroupNew.location
+resource_group_name = azurerm_resource_group.dvopsResourceGroupNew.name
+dns_prefix = "budget-buddy-aks"
 default_node_pool {
 name = "default"
 node_count = 1
 vm_size = "Standard_DS2_v2"
 }
 service_principal {
-client_id = "1da05597-76ec-4255-9d25-93a78b57def0"
-client_secret = "k7X8Q~aRCMQD3RVGVcVnDnUwhGl8v6oVU1WzxcNi"
+client_id = "204139ba-add0-4b65-8b7a-69d27e53ee00"
+client_secret = "Aof8Q~XXF_LYeIwkT3WVUAJE._naRnwcJMRKpdsw"
 }
 }
