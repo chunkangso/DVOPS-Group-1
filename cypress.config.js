652455d6953a669be669899178bae99cc83016e1
SentryCore <chunkangso@gmail.com>
2024-02-04 03:52:18 +0800

    Unit Testing left with 1 for 100% coverage

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
+      require('@cypress/code-coverage/task')(on, config);
       // include any other plugin code...
 
       // It's IMPORTANT to return the config object
@@ -13,4 +13,4 @@ module.exports = defineConfig({
       return config
     },
   },
-})
+});
